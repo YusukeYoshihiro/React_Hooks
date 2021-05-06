c8451f45e761007b708a066dc1e6793f278c8383
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-05-05 20:19:04 -0700

    Added Route and Link components  Adopted window.history.pushState({}, '', href)     const navEvent = new PopStateEvent('popstate');     window.dispatchEvent(navEvent) into Link.js to implement page transition by using Route on navigation part.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import React from 'react'
-// import ColorSelect from './components/ColorSelect';
-// import Dropdown from './components/Dropdown';
+import React from 'react';
+import Accordion from './components/Accordion';
+import ColorSelect from './components/ColorSelect';
 import Translate from './components/Translate';
-// import Search from './components/Search';
+import Search from './components/Search';
+import Route from './components/Route';
+import Header from './components/Header';
 
-// const items = [
-//   {
-//     title: 'What is React?',
-//     content: 'React is a front end javascript framework',
-//   },
-//   {
-//     title: 'Why use React?',
-//     content: 'React is a favorite JS library among engineers',
-//   },
-//   {
-//     title: 'How do you use React?',
-//     content: 'You use React by creating components',
+// const showAccordion = () => {
+//   if(window.location.pathname === '/'){
+//     return <Accordion items={items}/>
 //   }
-// ];
+// }
+
+// const showList = () => {
+//   if(window.location.pathname === '/list'){
+//     return <Search />
+//   }
+// } 
+
+// const showDropdown = () => {
+//   if(window.location.pathname === '/dropdown'){
+//     return <ColorDropdown />
+//   }
+// }
+
+// const showTranslate = () => {
+//   if(window.location.pathname === '/translate'){
+//     return <Translate />
+//   }
+// }
 
 const App = () => {
   return(
-  // <ColorSelect/>
   <div className="ui container" style={{marginTop:"10px"}}>
-  <Translate/>
+   <Header />
+   <Route path="/">
+     <Accordion />
+   </Route>
+   <Route path="/list">
+    <Search />
+   </Route>
+   <Route path="/select">
+     <ColorSelect /> 
+   </Route> 
+   <Route path="/translate">
+     <Translate />
+   </Route>
+  {/* {showAccordion()}
+  {showList()}
+  {showDropdown()}
+  {showTranslate()} */}
   </div>
   )}
 
